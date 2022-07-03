b5cd8c74bcfe83e39eea28be40bc503e3b19ba71
Victoria David <victoria@thedavids.us>
2022-07-03 13:33:26 -0400

    added submit function and prevented default behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");    //establish variables
 
-var createTaskHandler = function()                     //define function
+var createTaskHandler = function(event)                     //define function
 {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");     //create a new item
     listItemEl.className = "task-item";                //style the item
     listItemEl.textContent = "This is a new task.";    //add text
     tasksToDoEl.appendChild(listItemEl);               //add item to the end of the current task list
 };
 
-buttonEl.addEventListener("click", createTaskHandler); //call function based on an action
+formEl.addEventListener("submit", createTaskHandler);   //call function based on the submit action
 
